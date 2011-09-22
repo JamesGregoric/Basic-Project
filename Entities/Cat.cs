29795e0a50bcbedd06218c3a38f519798c7dc43e
James Gregoric <james.gregoric@directgeneral.com>
2011-09-22 13:25:07 -0500

    Entities commit

---
+++ Entities/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities
+{
+    public class Cat
+    {
+        private String _happy = "Meow";
+        private String _angry = "HISS";
+
+        public String CatIsHappy()
+        {
+            return _happy;
+        }
+
+        public String CatIsAngry()
+        {
+            return _angry;
+        }
+    }
+}
