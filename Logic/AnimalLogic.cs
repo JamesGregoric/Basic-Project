84c9465165ebff50c8c18ca0a85db4e099b41283
James Gregoric <james.gregoric@directgeneral.com>
2011-09-22 13:26:15 -0500

    Logic commit

---
+++ Logic/AnimalLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities;
+using Logic.Interface;
+
+namespace Logic
+{
+    public class AnimalLogic : IAnimalLogic
+    {
+        private Cat _cat = new Cat();
+
+        public String SeeCat(String mood)
+        {
+            switch (mood.Trim().ToUpper())
+            {
+                case "HAPPY":
+                    return _cat.CatIsHappy();
+                case "Angry":
+                    return _cat.CatIsAngry();
+                default:
+                    return "Please enter a valid mood";
+            }
+        }
+
+        public String SeeHappyCat()
+        {
+            return _cat.CatIsHappy();
+        }
+
+        public String SeeAngryCat()
+        {
+            return _cat.CatIsAngry();
+        }
+    }
+}
---
+++ Logic/Interface/IAnimalLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Interface
+{
+    public interface IAnimalLogic
+    {
+        #region Cat
+        String SeeCat(String mood);
+        String SeeHappyCat();
+        String SeeAngryCat();
+        #endregion
+    }
+}
