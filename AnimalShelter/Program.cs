6bcb9fcb5dde6d4dfa7d60a1fae2083e729043fd
James Gregoric <james.gregoric@directgeneral.com>
2011-09-22 13:22:47 -0500

    AnimalShelter commit

---
+++ AnimalShelter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Logic;
+using Logic.Interface;
+
+namespace AnimalShelter
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            IAnimalLogic _animalLogic = new AnimalLogic();
+
+            String happyCat = _animalLogic.SeeHappyCat();
+            Console.Write("You see a happy cat and it says " + happyCat );
+            Console.ReadLine();
+        }
+    }
+}
